6680ea1ec772b617d18867da4478849a534334e5
EungBug <eungb14@gmail.com>
2023-07-27 21:32:34 +0900

    style: 연차 등록 버튼 추가

--- src/pages/DayOff.tsx
+++ src/pages/DayOff.tsx
@@ -1,7 +1,9 @@
+import { useCallback } from 'react'
 import { DayOffSummary, DayOffRequestItem, DayOffHistorytItem } from 'components/index'
 import { styled } from 'styled-components'
 
-import { List } from 'antd'
+import { List, Button } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
 
 export interface DummyDayOffItem {
   id: number
@@ -29,8 +31,15 @@ const requestData: DummyDayOffItem[] = [
 ]
 
 export const DayOff = () => {
+  const handleClickAdd = useCallback(() => {}, [])
+
   return (
     <Container>
+      <ButtonBox>
+        <Button type="primary" icon={<PlusOutlined />} onClick={handleClickAdd}>
+          연차 등록하기
+        </Button>
+      </ButtonBox>
       <Wapper>
         <h2>나의 연차</h2>
         <DayOffSummary />
@@ -41,7 +50,7 @@ export const DayOff = () => {
           연차 신청 내역 <span>{requestData.length}</span>
         </h2>
         <List
-          pagination={{ position: 'bottom', align: 'end' }}
+          pagination={{ position: 'bottom', align: 'end', pageSize: 5 }}
           dataSource={requestData}
           renderItem={item => <DayOffRequestItem item={item} />}
         />
@@ -52,7 +61,7 @@ export const DayOff = () => {
           연차 사용 내역 <span>{requestData.length}</span>
         </h2>
         <List
-          pagination={{ position: 'bottom', align: 'end' }}
+          pagination={{ position: 'bottom', align: 'end', pageSize: 5 }}
           dataSource={requestData}
           renderItem={item => <DayOffHistorytItem item={item} />}
         />
@@ -90,4 +99,8 @@ const Wapper = styled.div`
       color: var(--color-green-1);
     }
   }
+`
+const ButtonBox = styled.div`
+  position: absolute;
+  right: 40px;
 `
