b7f74824157affa193a1938d4a6c2adb5c7eb668
EungBug <eungb14@gmail.com>
2023-08-03 20:47:59 +0900

    feat: 당직 상태 처리 시 푸시 알림 기능 추가

--- src/components/admin/DutyMgt.tsx
+++ src/components/admin/DutyMgt.tsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react'
 import type { ColumnsType } from 'antd/es/table'
 import { Table, Button } from 'antd'
 import axios from 'node_modules/axios/index'
+import { addDoc, collection } from 'firebase/firestore'
+import { notificationRef } from '@/firebase'
+import dayjs from 'dayjs'
+
 interface DataType {
   key: React.Key
   name: string
@@ -62,6 +66,19 @@ export const DutyMgt = () => {
   useEffect(() => {
     getScheduleList()
   }, [])
+
+  // 당직 승인 반려 알림 푸시
+  // *  호출 예시 >> pushDutyStatusNotification('REJECT')
+  const pushDutyStatusNotification = async (status: string) => {
+    const userNotiRef = collection(notificationRef, 'userid1', 'notiList')
+    await addDoc(userNotiRef, {
+      date: dayjs().format('YYYY-MM-DD'),
+      message: `당직 신청이 ${status === 'APPROVE' ? '승인' : '반려'}되었습니다.`,
+      status: status,
+      type: '당직',
+      read: false
+    })
+  }
 
   // const body = {
 
