190ee03afcd46048312249461495f72f222987fb
EungBug <eungb14@gmail.com>
2023-08-04 13:11:53 +0900

    fix: api interface 수정

--- src/apis/scheduleRequests.ts
+++ src/apis/scheduleRequests.ts
@@ -1,7 +1,7 @@
 import { client } from 'apis/index'
 import { IBaseResponse, IDataResponse, ICalendarUser } from 'types/index'
 
-export const getCalendarUserList = async (): Promise<IDataResponse<ICalendarUser>> => {
+export const getCalendarUserList = async (): Promise<IDataResponse<ICalendarUser[]>> => {
   const response = await client.get('/mypage/schedule/userList')
   return response.data
 }
