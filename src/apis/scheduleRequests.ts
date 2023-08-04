00f21b21df42818017661cc6ca18ba760913884b
EungBug <eungb14@gmail.com>
2023-08-04 16:14:10 +0900

    style: comment add

--- src/apis/scheduleRequests.ts
+++ src/apis/scheduleRequests.ts
@@ -10,6 +10,7 @@ export const getCalendarUserList = async (): Promise<IDataResponse<ICalendarUser
 export const insertDayOff = async (params: IDayOffRequest): Promise<IBaseResponse> => {
   const response = await client.post('/mypage/dayoff/register', params, {
     headers: {
+      // TODO : 토큰 값 수정
       Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyMDFAYWRtaW4uY29tIiwicm9sZSI6IuydvOuwmCIsImlkIjoxMiwiZXhwIjoxNjkxMjk4OTk5fQ.lu3OkypFGrYJ5BofmO89eE7T8r3sjLRpkdnUbUvuRsvKd5y7z5ImW-0ZPnUUEKn0Sf0Omon6EhJPsO5QlZ4DWQ`
     }
   })
