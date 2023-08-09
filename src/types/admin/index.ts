b74132c44940d462d18f0484bc5c950b9e453058
EungBug <eungb14@gmail.com>
2023-08-09 14:22:08 +0900

    fix: types index 수정

---
+++ src/types/admin/index.ts
@@ -0,0 +1,3 @@
+export * from 'types/admin/Employee'
+export * from 'types/admin/EmployeeUpdate'
+export * from 'types/admin/Schedule'
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,4 @@ export * from 'types/duty'
 export * from 'types/dayoff'
 export * from 'types/calendar'
 export * from 'types/admin'
-
 export * from 'types/IUser'
