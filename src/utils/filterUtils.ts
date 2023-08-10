67a1229608cf35dd83a1eada7d86729fa5116de4
EungBug <eungb14@gmail.com>
2023-08-10 13:19:22 +0900

    fix: utils typescript error fix

--- src/utils/filterUtils.ts
+++ src/utils/filterUtils.ts
@@ -5,72 +5,74 @@ import dayjs from 'dayjs'
 // * 신청 내역 필터링 기준
 // * 연차: 오늘 이전, 오전반차: 오늘날짜 9시 이전, 오후반차: 오늘 날짜 14시 이전
 export const getFilteredDayOffRequestList = (dayOffList: IDayOffResponse[]) => {
-  return (
-    dayOffList.filter(data => {
-      switch (data.type) {
-        case 0:
-          if (dayjs().startOf('date').diff(data.startDate) < 0) {
-            return data
-          }
-          break
-        case 1:
-          if (
-            dayjs().startOf('date').diff(data.startDate) < 0 ||
-            (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') < 9)
-          ) {
-            return data
-          }
-          break
-        case 2:
-          if (
-            dayjs().startOf('date').diff(data.startDate) < 0 ||
-            (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') < 14)
-          ) {
-            return data
-          }
-          break
-        default:
-          break
-      }
-    }) ?? []
-  )
+  const filteredList = [] as IDayOffResponse[]
+
+  dayOffList.map(data => {
+    switch (data.type) {
+      case 0:
+        if (dayjs().startOf('date').diff(data.startDate) < 0) {
+          filteredList.push(data)
+        }
+        break
+      case 1:
+        if (
+          dayjs().startOf('date').diff(data.startDate) < 0 ||
+          (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') < 9)
+        ) {
+          filteredList.push(data)
+        }
+        break
+      case 2:
+        if (
+          dayjs().startOf('date').diff(data.startDate) < 0 ||
+          (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') < 14)
+        ) {
+          filteredList.push(data)
+        }
+        break
+      default:
+        break
+    }
+  })
+  return filteredList.sort((a, b) => a.startDate.localeCompare(b.startDate))
 }
 
 // 사용 내역 필터링
 // * 사용 내역 필터 기준
 // * startDate가 오늘 날짜 이후면서 승인 상태인 경우
 export const getFilteredDayOffHistoryList = (dayOffList: IDayOffResponse[]) => {
-  return (
-    dayOffList
-      .filter(data => {
-        switch (data.type) {
-          case 0:
-            if (dayjs().startOf('date').diff(data.startDate) >= 0) {
-              return data
-            }
-            break
-          case 1:
-            if (
-              dayjs().startOf('date').diff(data.startDate) >= 0 ||
-              (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') >= 9)
-            ) {
-              return data
-            }
-            break
-          case 2:
-            if (
-              dayjs().startOf('date').diff(data.startDate) >= 0 ||
-              (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') >= 14)
-            ) {
-              return data
-            }
-            break
-          default:
-            break
+  const filteredList = [] as IDayOffResponse[]
+
+  dayOffList.map(data => {
+    switch (data.type) {
+      case 0:
+        if (dayjs().startOf('date').diff(data.startDate) >= 0) {
+          filteredList.push(data)
         }
-      })
-      .filter(data => data.status === 1) ?? []
-  )
+        break
+      case 1:
+        if (
+          dayjs().startOf('date').diff(data.startDate) >= 0 ||
+          (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') >= 9)
+        ) {
+          filteredList.push(data)
+        }
+        break
+      case 2:
+        if (
+          dayjs().startOf('date').diff(data.startDate) >= 0 ||
+          (dayjs().startOf('date').diff(data.startDate) === 0 && dayjs().get('hour') >= 14)
+        ) {
+          filteredList.push(data)
+        }
+        break
+      default:
+        break
+    }
+  })
+  return filteredList
+    .filter(data => data.status === 1)
+    .sort((a, b) => b.startDate.localeCompare(a.startDate))
 }
 
 // 당직 신청 내역 필터링
